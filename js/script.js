2c22abd0b0117866ed663ea32a72dba1586db66f
Darshan Bhadiyadra <129250327+Darshan-Bhadiyadra@users.noreply.github.com>
2024-02-28 12:45:17 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,28 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
+
+wow = new WOW(
+    {
+    boxClass:     'wow',      // default
+    animateClass: 'animated', // default
+    offset:       0,          // default
+    mobile:       true,       // default
+    live:         true        // default
+  }
+  )
+  wow.init();
+  AOS.init();
